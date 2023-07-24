5b279acc26dc55624b13f48113af5f93b8735863
Sichkar Yurii <si4kar2017@gmail.com>
2023-07-24 16:43:20 +0300

    server

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-import AppController from "./controller/AppController.mjs";
-
-const body = document.querySelector('.body');
-const wrapper = document.querySelector('.wrapper');
-const casesBlock = document.querySelector('.case-today');
-const shopping = document.querySelector('.case-shoping');
-const weather = document.querySelector('.case-weather');
-
-new AppController(body, wrapper, casesBlock, shopping, weather);
--- src/js/config.mjs
+++
@@ -1,3 +0,0 @@
-export const TODOS_URL = 'https://63b17044f9a53fa20277bc04.mockapi.io/todoList/';
-
-export const WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=&lon=&appid=08ea5a38d22551f446579fd0136e6518`;
